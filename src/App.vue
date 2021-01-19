98a720c7aa261f46d535e8e4b0ce784873fa5586
Torres92 <aleph2445@gmail.com>
2021-01-19 18:16:15 -0500

    commiting vue skill

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <Skills/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Skills from './components/Skills.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Skills
   }
 }
 </script>
 
 <style>
-#app {
+
+
+
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+ 
+ body {
+   background-color: #eeeeee;
+   font-family: 'Montserrat', sans-serif;
+   display: grid;
+   grid-template-rows: auto;
+   justify-items: center;
+   align-items: center;
+ }
+
+ body, html {
+   margin: 0;
+   height: 100%;
+ }
+
+ #app {
+   width: 50%;
+ }
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
+
+/* body {
+  background-color: #eeeeee;
+} */
 </style>
---
+++ src/components/Skills.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <form @submit.prevent="addSkill">
+        <!-- <Form v-slot="{ errors }">
+          <Field name="field" :rules="isRequired" />
+
+          <span>{{ errors.field }}</span> -->
+          <input type="text" placeholder="Enter a skill" v-model="skill" />
+        <!-- </Form> -->
+        <!-- <input type="checkbox" v-model="checked" id="checkbox"> -->
+      </form>
+      <ul>
+        <li v-for="(data, index) in skills" :key="index">
+          {{ index }}. {{ data.skill }}
+        </li>
+      </ul>
+
+      <!-- <div v-bind:class="{ alert: showAlert, 'another-class': showClass }"></div>
+       </div> -->
+
+      <!-- <div v-bind:class="alertObject"></div>
+       </div> -->
+
+      <p>These are the skills that you possess.</p>
+      <!-- 
+       <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight }"></div>
+       </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+// import { Field, Form } from "vee-validate";
+
+export default {
+  name: "Skills",
+  // components: {
+  //   Field,
+  //   Form,
+  // },
+  data() {
+    return {
+      // checked: false,
+
+      skill: "",
+      skills: [{ skill: "Vue.js" }, { skill: "Frontend Developer" }],
+      // showAlert: true,
+      // showClass: true,
+      // alertObject: {
+      //   alert: true
+      // }
+
+      // bgColor: "yellow",
+      // bgWidth: "100%",
+      // bgHeight: "30px",
+    };
+  },
+  methods: {
+     // Validator function
+    isRequired(value) {
+      return value ? true : 'This field is required';
+    },
+    addSkill() {
+      this.skills.push({ skill: this.skill }), (this.skill = "");
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.holder {
+  background: #ffffff;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  border-left: 5px solid #3eb3f6;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+
+p {
+  text-align: center;
+  padding: 30px 0;
+  color: gray;
+}
+
+.container {
+  box-shadow: 0px 0px 40px lightgray;
+}
+
+input {
+  width: calc(100% - 40px);
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #323333;
+  color: #687f7f;
+}
+</style>
